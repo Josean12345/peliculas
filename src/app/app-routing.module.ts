f27f10fc0fd1639b06f815bcc3ce8973b1ae2ca9
josean <coty382000@hotmail.com>
2023-09-14 12:54:42 +0200

    Mensaje descriptivo de tu commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
-
-import { MenuNavComponent } from './components//menu-nav/menu-nav.component';
-import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
-import { PeliculasComponent } from './components/peliculas/peliculas.component';
-import { NoticiasComponent } from './components/noticias/noticias.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { BuscarComponent } from './components/buscar/buscar.component';
-
-
-/* Soy el programado 2 */
 const routes: Routes = [
   {
     path: 'home', component: HomeComponent
   },
-  {
-    path: 'sobre-mi', component: SobreMiComponent
-  },
-  {
-    path: 'peliculas', component: PeliculasComponent
-  },
-  {
-    path: 'noticias', component: NoticiasComponent
-  },
-
-  {
-    path: 'contacto', component: ContactoComponent
-  },
-  {
-    path: 'buscar', component: BuscarComponent
-  },
+   
   {
     path: "", redirectTo: "home", pathMatch: "full"
 
